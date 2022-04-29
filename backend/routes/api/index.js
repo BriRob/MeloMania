96496d1acfa2008e3d1681e380ac161dd5fe6880
Briana Robinson <briana.n.robinson@gmail.com>
2022-04-29 17:09:28 -0400

    added backend and frontend authentication

---
+++ backend/routes/api/index.js
@@ -0,0 +1,57 @@
+const router = require("express").Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
+
+
+
+module.exports = router;
+
+// test api route with backend set up and partially for frontend set up to test csrfFetch
+// router.post("/test", function (req, res) {
+//   res.json({ requestBody: req.body });
+// });
+
+// API TESTING WITH AUTH MIDDLEWARE
+
+// const asyncHandler = require('express-async-handler');
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { restoreUser } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+
+// // test the setTokenCookie function by getting the demo user and calling setTokenCookie
+// // GET /api/set-token-cookie
+// router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+//   const user = await User.findOne({
+//     where: {
+//       username: 'Demo-lition'
+//     }
+//   });
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// }));
+
+// // test the restoreUser middleware by connecting the middleware and checking whether or not the req.user key has been populated by the middleware properly
+// // GET /api/restore-user
+// router.get(
+//   '/restore-user',
+//   restoreUser,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// // testing requireAuth middleware / if no session user, return error / otherwise, return session user's info
+// // GET /api/require-auth
+// const { requireAuth } = require('../../utils/auth.js');
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
