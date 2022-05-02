0b6cf7bb7c538577aaa7407a61a5a448984a9838
Briana Robinson <briana.n.robinson@gmail.com>
2022-05-02 11:14:35 -0400

    added audio player

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,26 +1,27 @@
+import ReactAudioPlayer from "react-audio-player";
 import React, { useState } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 
 function HomePage() {
-    const dispatch = useDispatch();
-    const sessionUser = useSelector((state) => state.session.user);
+  const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.session.user);
 
-    return (
+  return (
+    <div>
+      <h2 className="title">Welcome!</h2>
+      {/* <p>The perfect place to upload and share your music with the world</p> */}
+      {/* <div className="songsContainer">
+        <ReactAudioPlayer src="/Users/brirobin/Desktop/aA/Module-5/melomania-solo-project/frontend/public/songs/tadow-fkj-masego.mp3" controls/>
         <div>
-            <h2 className="title">Welcome!</h2>
-            <p>The perfect place to upload and share your music with the world</p>
-            <div className="songsContainer">
-                <div>
-                    <div>Tadow</div>
-                    <div>fkj</div>
-                    <span>4 days ago</span>
-                </div>
-
-            </div>
+          <div className="songTitle">Tadow</div>
+          <div className="uploadedUser">fkj</div>
+          <span className="detail-date">4 days ago</span>
         </div>
-    )
+  </div> */}
+    </div>
+  );
 }
 
 export default HomePage;
