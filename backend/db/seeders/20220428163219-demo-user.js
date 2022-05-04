3085bc6fad1d9e5be0b7cdc307af1a8c9905407b
Briana Robinson <briana.n.robinson@gmail.com>
2022-05-04 13:55:34 -0400

    fixed demo user seeders

--- backend/db/seeders/20220428163219-demo-user.js
+++ backend/db/seeders/20220428163219-demo-user.js
@@ -7,33 +7,33 @@ module.exports = {
       "Users",
       [
         {
-          email: "demo@user.io",
-          username: "Demo-lition",
-          hashedPassword: bcrypt.hashSync("password"),
+          email: 'demo@user.io',
+          username: 'Demo-Melomaniac',
+          hashedPassword: bcrypt.hashSync('password'),
           createdAt: new Date(),
           updatedAt: new Date()
-        },
-        {
-          email: "knowell@briana.io",
-          username: "knowell3",
-          hashedPassword: bcrypt.hashSync("password"),
+          },
+          {
+          email: 'knowell@briana.io',
+          username: 'knowell3',
+          hashedPassword: bcrypt.hashSync('knowEll3'),
           createdAt: new Date(),
           updatedAt: new Date()
-        },
-        {
-          email: "user1@user.io",
-          username: "FakeUser1",
-          hashedPassword: bcrypt.hashSync("password2"),
+          },
+          {
+          email: 'alfamist@user.io',
+          username: 'alfamist',
+          hashedPassword: bcrypt.hashSync('alfamistnocturne'),
           createdAt: new Date(),
           updatedAt: new Date()
-        },
-        {
-          email: "user2@user.io",
-          username: "FakeUser2",
-          hashedPassword: bcrypt.hashSync("password3"),
+          },
+          {
+          email: 'mereba@user.io',
+          username: 'mereba',
+          hashedPassword: bcrypt.hashSync('merebakin'),
           createdAt: new Date(),
           updatedAt: new Date()
-        },
+          },
       ],
       {}
     );
