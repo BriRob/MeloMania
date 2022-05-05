0ba54e43610df2cf37380dbcd19efa46f8dd3bdd
Briana Robinson <briana.n.robinson@gmail.com>
2022-05-05 00:23:30 -0400

    working playlist create functionality

--- backend/routes/api/playlists.js
+++ backend/routes/api/playlists.js
@@ -6,17 +6,19 @@ const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation.js");
 const { requireAuth } = require("../../utils/auth.js");
 
-
 // getting all playlists
-router.get("/", asyncHandler(async (req, res) => {
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
     // res.send("hello")
     const playlists = await Playlist.findAll({
-        // include: [User, Song],
-        include: [User, {model: Song, include: User}],
-        order: [["createdAt", "DESC"]]
-    })
-    return res.json(playlists)
-}));
+      // include: [User, Song],
+      include: [User, { model: Song, include: User }],
+      order: [["createdAt", "DESC"]],
+    });
+    return res.json(playlists);
+  })
+);
 
 // getting one playlist
 // router.get("/:id(\\d+)", asyncHandler(async (req, res) => {
@@ -28,12 +30,37 @@ router.get("/", asyncHandler(async (req, res) => {
 //     return res.json(playlist);
 // }));
 
-router.post("/new-playlist", requireAuth, asyncHandler(async (req, res) => {
+const playlistFormValidation = [
+  check("title")
+    .notEmpty()
+    .withMessage("Title cannot be empty")
+    .isLength({ max: 100 })
+    .withMessage("Title must be less than 100 characters")
+    .custom((value) => !/^ *$/.test(value))
+    .withMessage("Title must contain characters"),
+  handleValidationErrors,
+];
 
-}));
+router.post(
+  "/new-playlist",
+  requireAuth,
+  playlistFormValidation,
+  asyncHandler(async (req, res) => {
+    const userId = req.user.id;
+    const { title } = req.body;
+    const newPlaylist = await Playlist.create({ userId, title });
 
-router.delete("/:id(\\d+)", asyncHandler(async (req, res) => {
+    // making this returning playlist so that updated playlists state has the same eagerloading keys the other playlists in main state
+    const returningPlaylist = await Playlist.findByPk(newPlaylist.id, {
+      include: [User, { model: Song, include: User }],
+    });
+    return res.json(returningPlaylist);
+  })
+);
 
-}));
+router.delete(
+  "/:id(\\d+)",
+  asyncHandler(async (req, res) => {})
+);
 
 module.exports = router;
